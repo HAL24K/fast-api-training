b7c86e736907218fd74c52ee676ae0bb28961104
Bobby Drake <bobby.drake@hal24k.com>
2021-06-08 08:23:16 +0200

    update hello-world-api

---
+++ src/1-hello-world-api/test_api.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+
+def test_get_items():
+    pass
+
+
+def test_add_item():
+    pass
+
+
+def test_update_item():
+    pass
+
+
+def test_delete_item():
+    pass
